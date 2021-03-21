65f74f2a64b8302f21b2e389df8f0f0d146d4dbf
Jay <jaydeep.basu1@gmail.com>
2021-03-21 23:44:49 +0530

    updated with app.py to deploy the model

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Car Price Prediction
 
 An automobile company outisde US aspires to enter the US market by setting up their manufacturing unit there and producing cars locally to give competition to their US and European counterparts.
+
 They have contracted an automobile consulting company to understand the factors on which the pricing of cars depends. Specifically, they want to understand the factors affecting the pricing of cars in the American market, since those may be very different from the Chinese market. The company wants to know:
 Which variables are significant in predicting the price of a car How well those variables describe the price of a car Based on various market surveys, the consulting firm has gathered a large dataset of different types of cars across the Americal market.
+
 Business Goal - You are required to model the price of cars with the available independent variables. It will be used by the management to understand how exactly the prices vary with the independent variables. They can accordingly manipulate the design of the cars, the business strategy etc. to meet certain price levels. Further, the model will be a good way for management to understand the pricing dynamics of a new market.
 
 ## Data
@@ -19,14 +21,34 @@ The project was developed using python 3.8.3 with the following packages.
 - seaborn
 - Scikit-learn
 - statsmodels
+- Joblib
+- Streamlit
 
+
+Installation with pip:
+
+```bash
+pip install -r requirements.txt
+```
+
+
+## Getting Started
+Open the terminal in you machine and run the following command to access the web application in your localhost.
+```bash
+streamlit run app.py
+```
 
 ## Files
 - notebook/Car_Price_IMP_Feature_Prediction_LinearRegression_Ridge_and_Lasso.ipynb : Jupyter Notebook with all the workings including pre-processing, modelling using Multiple Linear Regression, further applying Ridge and Lasso Regularization and finally inference.
 
 - notebook/Car_Price_Prediction_XGBoost_Regression.ipynb : Jupyter Notebook with all the workings including pre-processing, modelling using XGBoost Regression to achieve better R2 score and inference.
 
+- app.py : Streamlit App script
+- requirements.txt : pre-requiste libraries for the project
+- models/ : trained model, scaler and one hot encoding objects
 - data/ : The source data and data dictionary.
+- setup.sh : Setup file for Heroku.
+- Procfile : To trigger the app in Heroku.
 
 
 ## Acknowledgements
---
+++ app.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import joblib
+import pandas as pd
+from PIL import Image
+from sklearn.preprocessing import StandardScaler
+
+st.set_page_config(page_title="Car Price Prediction App", page_icon=None, layout='centered', initial_sidebar_state='auto')
+
+# In[ ]:
+
+
+@st.cache(allow_output_mutation=True)
+def load(scaler_path, ohe_path, model_path):
+    sc = joblib.load(scaler_path)
+    ohe = joblib.load(ohe_path)
+    model = joblib.load(model_path)
+    return sc , ohe, model
+
+
+# In[ ]:
+
+
+def inference(row, cols, scaler, ohe, model):
+    df = pd.DataFrame([row], columns = cols)
+    
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    car_num_cols = list(df.select_dtypes(include=numerics).columns)
+    df[car_num_cols] = scaler.transform(df[car_num_cols])
+    
+    car_cat_cols = list(df.select_dtypes(exclude=numerics).columns)
+    car_ohe = ohe.transform(df[car_cat_cols])
+    car_df_ohe = pd.DataFrame(car_ohe, columns = ohe.get_feature_names(input_features = car_cat_cols))
+    
+    df = df.drop(car_cat_cols, axis=1)
+    df = pd.concat([df, car_df_ohe], axis=1)
+    
+    price = model.predict(df)[0]
+    
+    return price
+
+
+# In[ ]:
+
+
+st.title('Car Price Prediction App')
+st.write('Predicting Price of a Car based on its features')
+image = Image.open('data/car.jpg')
+st.image(image, use_column_width=True)
+st.write('Please fill in the details of the car under consideration in the left sidebar and click on the button below!')
+
+
+
+fuel_type =     st.sidebar.selectbox("Fuel Type", ('diesel','gas'))
+aspiration =     st.sidebar.selectbox("Aspiration", ('std','turbo'))
+door_number =     st.sidebar.selectbox("Door Number", ('two','four'))
+car_body =     st.sidebar.selectbox("Car Body", ('convertible','hardtop','hatchback','sedan','wagon'))
+drive_wheel =     st.sidebar.selectbox("Drive Wheel", ('rwd','fwd','4wd'))
+engine_location =     st.sidebar.selectbox("Engine Location", ('front','rear'))
+wheelbase =   st.sidebar.number_input("Wheel Base", 0.0, 130.0, 86.6, 1.0)
+carlength =   st.sidebar.number_input("Car Length", 0.0, 210.0, 141.1, 1.0)
+carwidth =   st.sidebar.number_input("Car Width", 0.0, 75.0, 60.3, 1.0)
+carheight =   st.sidebar.number_input("Car Length", 0.0, 60.0, 47.8, 1.0)
+curbweight =   st.sidebar.number_input("Curb Weight", 0, 4070, 1488, 100)
+engine_type =     st.sidebar.selectbox("Engine Type", ('dohc','dohcv','l','ohc','ohcf','ohcv','rotor'))
+cylinder_number =     st.sidebar.selectbox("Cylinder Number", ('two','three','four','five','six','eight','twelve'))
+enginesize =   st.sidebar.number_input("Engine Size", 0.0, 330.0, 61.0, 10.0)
+fuel_system =     st.sidebar.selectbox("Fuel System", ('1bbl','2bbl','4bbl','idi','mfi','mpfi','spdi','spfi'))
+boreratio =   st.sidebar.number_input("Bore Ratio", 0.0, 4.0, 2.54, 0.1)
+stroke =   st.sidebar.number_input("Stroke", 0.0, 4.5, 2.07, 0.1)
+compression_ratio =   st.sidebar.number_input("Compression Ratio", 0.0, 23.0, 7.0, 0.5)
+horsepower =       st.sidebar.slider("Horsepower", 0, 300, 48, 1)
+peakrpm =       st.sidebar.slider("Peak RPM", 0, 6600, 4150, 1)
+citympg =       st.sidebar.slider("City MPG", 0, 49, 13, 1)
+highwaympg =       st.sidebar.slider("Highway MPG", 0, 54, 16, 1)
+
+
+
+row = [fuel_type, aspiration, door_number, car_body, drive_wheel, engine_location, wheelbase, carlength, carwidth, carheight, curbweight, engine_type, cylinder_number, enginesize, fuel_system, boreratio, stroke, compression_ratio, horsepower, peakrpm, citympg, highwaympg]
+
+
+cols = ['fueltype', 'aspiration',
+   'doornumber', 'carbody', 'drivewheel', 'enginelocation', 'wheelbase',
+   'carlength', 'carwidth', 'carheight', 'curbweight', 'enginetype',
+   'cylindernumber', 'enginesize', 'fuelsystem', 'boreratio', 'stroke',
+   'compressionratio', 'horsepower', 'peakrpm', 'citympg', 'highwaympg']
+
+# In[ ]:
+
+
+if (st.button('Predict Car Price')):    
+    sc, ohe, model = load('models/scaler.joblib', 'models/ohe.joblib', 'models/XGBoost.joblib')
+    result = inference(row, cols, sc, ohe, model)
+    st.write(result)
